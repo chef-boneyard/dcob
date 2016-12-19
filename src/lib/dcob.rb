195248498dc9010c1a33ba7087904239e12ec0d7
Robb Kidd <robb@thekidds.org>
2016-12-19 12:53:56 -0500

    set a rubocop config; do the frozen string thing
    See
    http://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/FrozenStringLiteralComment
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- src/lib/dcob.rb
+++ src/lib/dcob.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "dcob/version"
 require "sinatra"
 require "octokit"
--- src/lib/dcob/version.rb
+++ src/lib/dcob/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Dcob
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.0"
 end
