f54a9b64e0a1b2c3e73572fc4d3b3ebbc0413691
Thom May <thom@chef.io>
2017-01-20 12:19:58 +0000

    Use the community engineering habitat origin
    Signed-off-by: Thom May <thom@may.lt>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=dcob
-pkg_origin=core
+pkg_origin=chef_community_engineering
 pkg_version=_computed_in_a_function_below_
 pkg_description="This is a github bot to ensure every commit on a PR has the
   Signed-off-by attribution required by the Developer Certificate of Origin."
